e31fb02b72cfac98df3feb37ceb663147fcddb52
Heondeuk Lee <leehd1995@gmail.com>
2024-10-29 19:21:28 +0900

    fix: bash file

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ export WANDB_PROJECT="judge"
 # 그 후에 원래 명령어 실행
 ACCELERATE_USE_FSDP=1 FSDP_CPU_RAM_EFFICIENT_LOADING=1 \
 torchrun --nproc_per_node=4 \
-./judge_test.py \
+./train.py \
 --config ./configs/base.yaml
